c20d5cfa55e59089cd0ec719f1aba10820be3eaa
chngchinboon <chinboon@outlook.com>
2018-02-08 20:10:06 +0800

    adding files

---
+++ app/src/main/java/nus/arduino2androidapp/DeviceList.java
@@ -0,0 +1,13 @@
+package nus.arduino2androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+}
